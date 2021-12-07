6cbe278dfb58eb69fcbbdafbf0f4e86609833017
segwuonwu <segwuonwu@gmail.com>
2021-12-06 18:04:40 -0800

    adding style to the app and getting the html elemets from the js file

---
+++ script.js
@@ -0,0 +1,12 @@
+//Getting the html elements
+let input = document.getElementById('cityinput');
+let submit = document.getElementById('add');
+let city = document.getElementById('city');
+let condition = document.getElementById('condition');
+let temp = document.getElementById('temp');
+let wind = document.getElementById('wind');
+
+//weather api
+api = "3045dd712ffe6e702e3245525ac7fa38";
+
+//converstion: 1 Kelvin = -272.15 Celsius
